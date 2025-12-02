b11a16f1339b44430b3dfac53982b5d8f410ba17
Rohan Pawar <pawarrohan2020@gmail.com>
2025-12-02 20:48:35 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",      
-    database: "permissions", 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
 });
